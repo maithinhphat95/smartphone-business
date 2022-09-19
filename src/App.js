e739ff4cef63be70cc4e90fede86a767cfeaafef
maithinhphat95 <maithinhphat.tdh@gmail.com>
2022-09-20 04:18:34 +0700

    [#3] Merge header and menu to admin page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./layouts/header";
-
+import Header from "./layouts/Header";
+import Menu from "./components/Menu";
 function App() {
   return (
     <div className="App">
       <Header />
+      <Menu />
     </div>
   );
 }
--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Title from "../../components/Title";
+import Title from "../Title";
 import { Box, Avatar, Typography } from "@mui/material";
-import AccountShort from "../../components/Account-short";
-import NavList from "../../components/NavList";
+import AccountShort from "../Account-short";
+import NavList from "../NavList";
 Menu.propTypes = {};
 
 function Menu(props) {
