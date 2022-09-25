d31d1c00d97984b560706ac9e0d7f8d365424b21
maithinhphat95 <maithinhphat.tdh@gmail.com>
2022-09-26 02:50:29 +0700

    [#25] init order table

---
+++ src/components/admin/Tables/OrderTable/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+OrderTable.propTypes = {};
+
+function OrderTable(props) {
+  return <div></div>;
+}
+
+export default OrderTable;
